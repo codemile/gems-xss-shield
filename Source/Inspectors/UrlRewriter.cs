d76d044f4d4921629def05738ce6172ec79f7f8c
thinkingmedia <mathew@thinkingmedia.ca>
2014-05-12 22:15:39 -0400

    testing.

--- Source/Inspectors/UrlRewriter.cs
+++ Source/Inspectors/UrlRewriter.cs
@@ -1,29 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
 using HtmlAgilityPack;
 
 namespace XssShield.Inspectors
 {
     /// <summary>
     /// Validates and rewrites the URL for attributes that can only accept a valid URL.
+    /// NOTE: This rewriter strips username and passwords from URLs.
     /// </summary>
     public class UrlRewriter : AttributeInspector, iInspector
     {
+        /// <summary>
+        /// </summary>
         public static readonly
-            Dictionary<string, IEnumerable<string>> Basic =
-                new Dictionary<string, IEnumerable<string>>
+            AttributeList Basic =
+                new AttributeList
                 {
-                    {"a", new[] {"href"}},
-                    {"img", new[] {"src"}},
-                    {"q", new[] {"cite"}}
+                    {"a", new List<string> {"href"}},
+                    {"img", new List<string> {"src"}},
+                    {"q", new List<string> {"cite"}}
                 };
+
+        /// <summary>
+        /// Reject URLs that use an IP address.
+        /// </summary>
+        private readonly bool _allowIPAddresses;
 
         /// <summary>
         /// Constructor
         /// </summary>
-        public UrlRewriter(AttributeList pWhiteList)
+        public UrlRewriter(AttributeList pWhiteList, bool pAllowIPAddresses)
             : base(pWhiteList)
         {
+            _allowIPAddresses = pAllowIPAddresses;
         }
 
         /// <summary>
@@ -33,22 +45,20 @@ namespace XssShield.Inspectors
         /// <returns>A new instance of BlackListed, or Null.</returns>
         public Rejection Inspect(HtmlNode pNode)
         {
+            if (pNode == null)
+            {
+                throw new NullReferenceException("pNode");
+            }
+
+            if (!InspectNode(pNode))
+            {
+                return null;
+            }
+
             string name = pNode.Name.ToLower().Trim();
-
-            if (pNode.NodeType != HtmlNodeType.Element
-                || !pNode.HasAttributes)
-            {
-                return null;
-            }
-
-            if (!Attributes.ContainsKey(name))
-            {
-                return null;
-            }
-
             foreach (string attrName in Attributes[name])
             {
-                string url = CleanURL(pNode.Attributes[attrName].Value);
+                string url = CleanURL(pNode.Attributes[attrName].Value, _allowIPAddresses);
                 if (url == null)
                 {
                     return new Rejection(true, new RiskDiscovery(pNode.Line, pNode.LinePosition, "Malformed URL"));
@@ -63,11 +73,99 @@ namespace XssShield.Inspectors
         /// Sanitizes and rewrites a URL
         /// </summary>
         /// <param name="pUrl">The input URL</param>
-        /// <returns>A new URL output</returns>
-        public static string CleanURL(string pUrl)
+        /// <param name="pAllowIP">True to allow IP addresses.</param>
+        /// <returns>A new URL, or Null if the input URL was invalid</returns>
+        public static string CleanURL(string pUrl, bool pAllowIP)
         {
-            Uri uri = new Uri(pUrl);
-            return uri.ToString();
+            if (pUrl == null)
+            {
+                throw new NullReferenceException("pUrl");
+            }
+
+            try
+            {
+                Uri uri = new Uri(pUrl);
+
+                // only allow HTTP schemes
+                if (uri.Scheme != Uri.UriSchemeHttp && uri.Scheme != Uri.UriSchemeHttps)
+                {
+                    return null;
+                }
+
+                // only allow DNS and IP hosts
+                if (uri.HostNameType == UriHostNameType.Unknown
+                    || uri.HostNameType == UriHostNameType.Basic)
+                {
+                    return null;
+                }
+
+                // optionally reject IP addresses
+                if (!pAllowIP &&
+                    (uri.HostNameType == UriHostNameType.IPv4 || uri.HostNameType == UriHostNameType.IPv6))
+                {
+                    return null;
+                }
+
+                // rebuild the URL by decoding and encoding it's components.
+                StringBuilder sb = new StringBuilder();
+                sb.Append(uri.Scheme);
+                sb.Append("://");
+                sb.Append(uri.Host.ToLower());
+
+                if (!uri.IsDefaultPort)
+                {
+                    sb.Append(":");
+                    sb.Append(uri.Port.ToString(CultureInfo.InvariantCulture));
+                }
+
+                sb.Append("/");
+
+                if (uri.Segments.Length > 0)
+                {
+                    string path = string.Join("/",
+                        uri.Segments.Select(pSeg=>pSeg.Replace("/", "")).Where(pSeg=>pSeg != ""));
+                    sb.Append(path);
+                }
+
+                if (!string.IsNullOrWhiteSpace(uri.Query))
+                {
+                    sb.Append(uri.Query);
+                }
+
+                if (!string.IsNullOrWhiteSpace(uri.Fragment))
+                {
+                    sb.Append("#");
+                    sb.Append(uri.Fragment.Substring(1));
+                }
+
+                return sb.ToString();
+            }
+            catch (UriFormatException)
+            {
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Checks if this node should be inspected.
+        /// </summary>
+        /// <param name="pNode">The node to inspect</param>
+        /// <returns>True to continue processing it, or False to skip it.</returns>
+        public bool InspectNode(HtmlNode pNode)
+        {
+            if (pNode == null)
+            {
+                throw new NullReferenceException("pNode");
+            }
+
+            if (pNode.NodeType != HtmlNodeType.Element
+                || !pNode.HasAttributes)
+            {
+                return false;
+            }
+
+            return Attributes.ContainsKey(pNode.Name.ToLower());
         }
     }
 }
--- Tests/Inspectors/UrlRewriterTests.cs
+++ Tests/Inspectors/UrlRewriterTests.cs
@@ -1,26 +1,138 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using HtmlAgilityPack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XssShield.Inspectors;
 
 namespace XssShieldTests.Inspectors
 {
     [TestClass]
     public class UrlRewriterTests
     {
-        [TestMethod]
-        public void CleanURLTest()
+        private void CleanList(Dictionary<string, string> pList)
         {
-            Assert.Fail();
+            foreach (KeyValuePair<string, string> pair in pList)
+            {
+                string url = UrlRewriter.CleanURL(pair.Key, false);
+                Assert.IsNotNull(url, pair.Key);
+                Assert.AreEqual(pair.Value, url);
+            }
         }
 
         [TestMethod]
-        public void InspectTest()
+        [ExpectedException(typeof (NullReferenceException))]
+        public void Clean_1()
         {
-            Assert.Fail();
+            UrlRewriter.CleanURL(null, false);
         }
 
         [TestMethod]
-        public void UrlRewriterTest()
+        public void Clean_2()
         {
-            Assert.Fail();
+            Dictionary<string, string> compare = new Dictionary<string, string>
+                                                 {
+                                                     {"http://www.thinkingmedia.ca", "http://www.thinkingmedia.ca/"},
+                                                     {
+                                                         "http://www.THINKINGMEDIA.ca/about/things",
+                                                         "http://www.thinkingmedia.ca/about/things"
+                                                     }
+                                                 };
+            CleanList(compare);
+        }
+
+        [TestMethod]
+        public void Clean_3()
+        {
+            Dictionary<string, string> compare = new Dictionary<string, string>
+                                                 {
+                                                     {
+                                                         "http://www.THINKINGMEDIA.ca/about us",
+                                                         "http://www.thinkingmedia.ca/about%20us"
+                                                     },
+                                                     {
+                                                         "http://www.thinking.ca:8080/index.php?find=some thing&that thing",
+                                                         "http://www.thinking.ca:8080/index.php?find=some%20thing&that%20thing"
+                                                     }
+                                                 };
+            CleanList(compare);
+        }
+
+        [TestMethod]
+        public void Clean_4()
+        {
+            Dictionary<string, string> compare = new Dictionary<string, string>
+                                                 {
+                                                     {
+                                                         "https://mathew:password@www.thinking.ca",
+                                                         "https://www.thinking.ca/"
+                                                     },
+                                                     {
+                                                         "http://mathew%40yaoo.com:password@www.thinking.ca/#thing",
+                                                         "http://www.thinking.ca/#thing"
+                                                     },
+                                                 };
+            CleanList(compare);
+        }
+
+        [TestMethod]
+        public void Clean_5()
+        {
+            List<string> xssUrls = new List<string>
+                                   {
+                                       "javascript:alert('hello');",
+                                       "JaVaScRiPt:alert('XSS')",
+                                       "javascript:alert(\"RSnake says, 'XSS'\")",
+                                       "#",
+                                       "&#106;&#97;&#118;&#97;&#115;&#99;&#114;&#105;&#112;&#116;&#58;&#97;&#108;&#101;&#114;&#116;&#40;&#39;&#88;&#83;&#83;&#39;&#41;",
+                                       "&#0000106&#0000097&#0000118&#0000097&#0000115&#0000099&#0000114&#0000105&#0000112&#0000116&#0000058&#0000097&#0000108&#0000101&#0000114&#0000116&#0000040&#0000039&#0000088&#0000083&#0000083&#0000039&#0000041",
+                                       "&#x6A&#x61&#x76&#x61&#x73&#x63&#x72&#x69&#x70&#x74&#x3A&#x61&#x6C&#x65&#x72&#x74&#x28&#x27&#x58&#x53&#x53&#x27&#x29",
+                                       "jav\tascript:alert('XSS');",
+                                       "jav&#x0A;ascript:alert('XSS');",
+                                       "jav&#x0D;ascript:alert('XSS');",
+                                       " &#14;  javascript:alert('XSS');",
+                                       "vbscript:msgbox(\"XSS\")",
+                                       "livescript:[code]",
+                                       "&{alert('XSS')}",
+                                       "xss:expression(alert('XSS'))",
+                                       "behavior: url(xss.htc);",
+                                       "¼script¾alert(¢XSS¢)¼/script¾"
+                                   };
+
+            foreach (string bad in xssUrls)
+            {
+                Assert.IsNull(UrlRewriter.CleanURL(bad, false));
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (NullReferenceException))]
+        public void Construct_1()
+        {
+            UrlRewriter url = new UrlRewriter(null, false);
+        }
+
+        [TestMethod]
+        public void Construct_2()
+        {
+            UrlRewriter url = new UrlRewriter(UrlRewriter.Basic, false);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (NullReferenceException))]
+        public void InspectNode_1()
+        {
+            UrlRewriter url = new UrlRewriter(UrlRewriter.Basic, false);
+            url.InspectNode(null);
+        }
+
+        [TestMethod]
+        public void InspectNode_2()
+        {
+            UrlRewriter url = new UrlRewriter(UrlRewriter.Basic, false);
+            Assert.IsTrue(url.InspectNode(HtmlNode.CreateNode("<img src=''/>")));
+            Assert.IsTrue(url.InspectNode(HtmlNode.CreateNode("<a href='#'>This is a test</a>")));
+            Assert.IsFalse(url.InspectNode(HtmlNode.CreateNode("<span>")));
+            Assert.IsFalse(url.InspectNode(HtmlNode.CreateNode("<span src=''></span>")));
         }
     }
 }
